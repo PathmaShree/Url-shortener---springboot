1030ed4b82933124156c01b6919c07bc6fe603d2
PathmaShree <146501513+PathmaShree@users.noreply.github.com>
2025-11-24 18:12:49 +0530

    Remove getters, setters, and overrides from UrlResponseDto

--- Backend/src/main/java/com/example/url/dto/UrlResponseDto.java
+++ Backend/src/main/java/com/example/url/dto/UrlResponseDto.java
@@ -21,51 +21,5 @@ public class UrlResponseDto {
 	private LocalDateTime expireAt;
 	private String shortCode;
 	
-	public String getShortCode() {
-		return shortCode;
-	}
-	public void setShortCode(String shortCode) {
-		this.shortCode = shortCode;
-	}
-	public String getShortUrl() {
-		return shortUrl;
-	}
-	public void setShortUrl(String shortUrl) {
-		this.shortUrl = shortUrl;
-	}
-	public String getOriginalUrl() {
-		return originalUrl;
-	}
-	public void setOriginalUrl(String originalUrl) {
-		this.originalUrl = originalUrl;
-	}
-	public LocalDateTime getExpireAt() {
-		return expireAt;
-	}
-	public void setExpireAt(LocalDateTime expireAt) {
-		this.expireAt = expireAt;
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(expireAt, originalUrl, shortUrl);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		UrlResponseDto other = (UrlResponseDto) obj;
-		return Objects.equals(expireAt, other.expireAt) && Objects.equals(originalUrl, other.originalUrl)
-				&& Objects.equals(shortUrl, other.shortUrl);
-	}
-	@Override
-	public String toString() {
-		return "UrlResponseDto [shortUrl=" + shortUrl + ", originalUrl=" + originalUrl + ", expireAt=" + expireAt + "]";
-	}
-	
-	
 	
 }
